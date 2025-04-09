e3f800e80ec4b89cdbf83077fc32b286eb6821c3
ChrlyDev <cf9220543@gmail.com>
2025-04-08 20:29:55 -0500

    "Crear Metodos HTTP"

--- src/routes/animal.js
+++ src/routes/animal.js
@@ -1,13 +1,53 @@
 const express = require("express");
 const router = express.Router(); //manejador de rutas de express
-const animalSchema = require("../models/animal"); 
+const animalSchema = require("../models/animal");
 
 //Nuevo animal
-router.post("/animals", (req, res) => {
+router.post("/create-animals", (req, res) => {
   const animal = animalSchema(req.body);
   animal
     .save()
     .then((data) => res.json(data))
     .catch((error) => res.json({ message: error }));
 });
+
+//Consultar un animal por id
+router.get("/find-animals/:id", (req, res) => {
+  const { id } = req.params;
+  animalSchema
+    .findById(id)
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+});
+
+//Consultar un animal por nombre
+router.get("/find-name-animals/:nombre", (req, res) => {
+  const { nombre } = req.params;
+
+  animalSchema
+    .findOne({ nombre })
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error.message }));
+});
+
+//Modificar un animal por id
+router.put("/update-animals/:id", (req, res) => {
+  const { id } = req.params;
+  const { nombre, edad, tipo, fecha } = req.body;
+
+  animalSchema
+    .updateOne({ _id: id }, { $set: { nombre, edad, tipo, fecha } })
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+});
+
+//Eliminar un animal por id
+router.delete("/delete-animals/:id", (req, res) => {
+  const { id } = req.params;
+  animalSchema
+    .findByIdAndDelete(id)
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+});
+
 module.exports = router;
